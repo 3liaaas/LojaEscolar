b749cb6bae41eea31110e4e0cf739cca08c972a8
Elias Araujo Rodrigues <contato.elias021021@gmail.com>
2026-05-25 20:00:54 -0300

    Iniciando projeto de loja escolar

---
+++ src/main/java/br/com/lojaescolar/eliasrodrigues/Main.java
@@ -0,0 +1,15 @@
+package br.com.lojaescolar.eliasrodrigues;
+
+public class Main {
+    public static void main(String [] args){
+        /*
+        * Classes, Objetos, Atributos, Métodos, Herança, Polimorfismo
+        * Classe abstratas e Métodos abstrato
+        * interfaces e Enum
+        * Records
+        * Coleções
+        * */
+        System.out.println("chama");
+    }
+
+}
---
+++ src/main/java/br/com/lojaescolar/eliasrodrigues/Produto.java
@@ -0,0 +1,27 @@
+package br.com.lojaescolar.eliasrodrigues;
+
+public class Produto {
+    private String nome; // <- atributo da classe Produto
+    private double preco;// <- atributo da classe Produto
+
+    public Produto(String nome, double preco){ // <-- Construtor que recebe ESTES 2 atributos!
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
